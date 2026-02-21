7f096a81cc712b1ca8f44a751c735ad69c9bd9d6
MyatBhoneThet <myatbhonethet01@gmail.com>
2026-02-21 23:05:16 +0700

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,27 +2,28 @@ import { useState } from "react";
 import ToggleAuth from "../components/Login/ToggleAuth";
 import EmailField, { PasswordField } from "../components/Login/InputField";
 import OAuthButtons from "../components/Login/OAuthButtons";
-import safeImg from "../assets/pill.jpg";
+import safeImg from "../assets/pill.gif";
 
 export default function Login() {
     const [mode, setMode] = useState("signin");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [wobble, setWobble] = useState(false);
 
     // Simple email validation
     const validateEmail = (value) => {
-    if (!value.includes("@") || !value.includes(".")) {
-      setError("Invalid email address");
-      return false;
-    }
-    setError("");
-    return true;
-  };
+        if (!value.includes("@") || !value.includes(".")) {
+            setError("Invalid email address");
+            return false;
+        }
+        setError("");
+        return true;
+    };
 
   // Continue button handler
     const handleSubmit = () => {
-        if (!validateEmail(email)) {
+        if (!validateEmail(email) && password.length < 7) {
             setWobble(true);
             setTimeout(() => setWobble(false), 500);
             return;
@@ -34,9 +35,13 @@ export default function Login() {
         <div className="min-h-screen flex">
             <div className={`w-full lg:w-1/2 flex items-center justify-center bg-white ${wobble ? "animate-wobble" : ""}`}>
                 <div className="w-[90%] max-w-md">
+
                     <h1 className="text-3xl font-bold mb-2">SmartSave</h1>
+
                     <h2 className="text-2xl font-semibold">Welcome Back</h2>
+
                     <p className="text-gray-400 mb-6">Please enter your details</p>
+
                     <ToggleAuth mode={mode} setMode={setMode} />
                     <EmailField
                         value={email}
@@ -44,11 +49,12 @@ export default function Login() {
                         error={error}
                     />
                     <PasswordField
-                        value={email}
+                        value={password}
                         type="password"
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={(e) => setPassword(e.target.value)}
                         error={error}
                     />
+
                     <button
                         onClick={handleSubmit}
                         className="w-full bg-blue-800 text-white py-3 rounded-xl mt-4 hover:scale-105 transition"
@@ -62,7 +68,7 @@ export default function Login() {
                     </div>
                 </div>
 
-                <div className="hidden lg:flex w-1/2 items-center justify-center bg-gradient-to-r from-blue-100 to-blue-300">
+                <div className="hidden lg:flex w-1/2 items-center justify-center">
                     <img src={safeImg} alt="safe" className="w-2/3" />
                 </div>
             </div>
